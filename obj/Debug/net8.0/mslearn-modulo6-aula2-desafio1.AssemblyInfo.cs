08e9c11a737f740f2c6e4e4afc696c4c6df8cff0
Wagner <wagner_silvajr@hotmail.com>
2024-04-29 23:21:50 -0300

    Test de depuração com diversos excepetion

--- obj/Debug/net8.0/mslearn-modulo6-aula2-desafio1.AssemblyInfo.cs
+++ obj/Debug/net8.0/mslearn-modulo6-aula2-desafio1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mslearn-modulo6-aula2-desafio1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0dace3ba9d535666a1e58636cc8b690e91acb30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f40cadb0670a7a38f6f6d0c35dd39847b2b0f313")]
 [assembly: System.Reflection.AssemblyProductAttribute("mslearn-modulo6-aula2-desafio1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mslearn-modulo6-aula2-desafio1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
